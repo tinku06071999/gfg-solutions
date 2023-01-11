d9d8f51f6c873cb787478978a3f45849856f7f31
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-01-11 22:10:10 +0530

    Updated Solution - GfG to GitHub

--- Medium/Make array elements unique/make-array-elements-unique.cpp
+++ Medium/Make array elements unique/make-array-elements-unique.cpp
@@ -11,16 +11,44 @@ class Solution {
   public:
     long long int minIncrements(vector<int> arr, int N) {
         // Code here
+        // sort(arr.begin(),arr.end());
+        // long long count = 0;
+        //  if(N==0)return 0;
+        //  for(int i=1;i<N;i++){
+        //      if(arr[i-1]>=arr[i]){
+        //          count += arr[i-1]-arr[i]+1;
+        //          arr[i]=arr[i-1]+1;
+        //  }
+        // }
+        // return count;
+         //     sort(arr.begin(),arr.end());
+    //     int count =0;
+    //     int i = 0;
+    //     while(i<N){
+    //         if(arr[i]==arr[i+1]){
+    //             arr[i+1]=arr[i+1]+1;
+    //             count++;
+    //             // i++;
+    //         }
+    //         else i++;
+    //     }
+    //     return count;
+    // }
+        // Code here
         sort(arr.begin(),arr.end());
-        long long count = 0;
-         if(N==0)return 0;
-         for(int i=1;i<N;i++){
-             if(arr[i-1]>=arr[i]){
-                 count += arr[i-1]-arr[i]+1;
-                 arr[i]=arr[i-1]+1;
-         }
+        
+        int prev = arr[0];
+        int ans = 0;
+        for(int i=1; i<N; i++)
+        {
+            if(arr[i]>prev) prev = arr[i];
+            else
+            {
+                ans+= (prev + 1 - arr[i]);
+                prev++;
+            }
         }
-        return count;
+        return ans;
     }
 };
 
