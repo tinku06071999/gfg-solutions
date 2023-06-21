cb5f859d59907bfb5f5eabad7b77767324f5e8a2
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-06-21 16:33:35 +0530

    Updated Solution - GfG to GitHub

--- Medium/Flood fill Algorithm/flood-fill-algorithm.cpp
+++ Medium/Flood fill Algorithm/flood-fill-algorithm.cpp
@@ -5,49 +5,74 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-    void bfs(int row, int col, vector<vector<int>>&vis,vector<vector<int>>&image, int n ,int m,int prevcolor,int color){
-      vis[row][col]=1;
-      queue<pair<int,int>>q;
-      q.push({row,col});
-      while(!q.empty()){
-          int row = q.front().first;
-          int col = q.front().second;
-          q.pop();
-            // if(image[row][col]) 
-            if(image[row][col]==prevcolor){
-                   image[row][col]=color;
-            }
-            if(row-1>=0 && !vis[row-1][col]&& image[row-1][col]==prevcolor){
-                 vis[row-1][col]=1;
-                   q.push({row-1,col});
-                   image[row-1][col]=color;
-            }
-             if(col+1<m && !vis[row][col+1]&& image[row][col+1]==prevcolor){
-                 vis[row][col+1]=1;
-                   q.push({row,col+1});
-                   image[row][col+1]=color;
-            }
-            if(row+1<n && !vis[row+1][col]&& image[row+1][col]==prevcolor){
-                 vis[row+1][col]=1;
-                   q.push({row+1,col});
-                   image[row+1][col]=color;
-            }
-            if(col-1>=0 && !vis[row][col-1]&& image[row][col-1]==prevcolor){
-                 vis[row][col-1]=1;
-                   q.push({row,col-1});
-                   image[row][col-1]=color;
-            }
+//     void bfs(int row, int col, vector<vector<int>>&vis,vector<vector<int>>&image, int n ,int m,int prevcolor,int color){
+//       vis[row][col]=1;
+//       queue<pair<int,int>>q;
+//       q.push({row,col});
+//       while(!q.empty()){
+//           int row = q.front().first;
+//           int col = q.front().second;
+//           q.pop();
+//             // if(image[row][col]) 
+//             if(image[row][col]==prevcolor){
+//                   image[row][col]=color;
+//             }
+//             if(row-1>=0 && !vis[row-1][col]&& image[row-1][col]==prevcolor){
+//                  vis[row-1][col]=1;
+//                   q.push({row-1,col});
+//                   image[row-1][col]=color;
+//             }
+//              if(col+1<m && !vis[row][col+1]&& image[row][col+1]==prevcolor){
+//                  vis[row][col+1]=1;
+//                   q.push({row,col+1});
+//                   image[row][col+1]=color;
+//             }
+//             if(row+1<n && !vis[row+1][col]&& image[row+1][col]==prevcolor){
+//                  vis[row+1][col]=1;
+//                   q.push({row+1,col});
+//                   image[row+1][col]=color;
+//             }
+//             if(col-1>=0 && !vis[row][col-1]&& image[row][col-1]==prevcolor){
+//                  vis[row][col-1]=1;
+//                   q.push({row,col-1});
+//                   image[row][col-1]=color;
+//             }
            
+//       }
+//   }
+  void dfs(int sr,int sc, vector<vector<int>>& ans,vector<vector<int>>& image,int delrow[],int delcol[],int inicolor, int color){
+      ans[sr][sc] = color;
+         int n=image.size();
+        int m = image[0].size();
+      for(int i=0;i<4;i++){
+          int nrow = sr+delrow[i];
+          int ncol = sc+delcol[i];
+          if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==inicolor && ans[nrow][ncol]!=color){
+              dfs(nrow,ncol,ans,image,delrow,delcol,inicolor,color);
+          }
       }
+
   }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n=image.size();
-        int m = image[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-             int prevcolor = image[sr][sc];
-                bfs(sr,sc,vis,image,n,m,prevcolor,color);
-        return image;
+        // int n=image.size();
+        // int m = image[0].size();
+        // vector<vector<int>>vis(n,vector<int>(m,0));
+        //      int prevcolor = image[sr][sc];
+        //         bfs(sr,sc,vis,image,n,m,prevcolor,color);
+        // return image;
+        
+        
+        // using dfs
+         vector<vector<int>>ans = image;
+        int inicolor = image[sr][sc];
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,+1,0,-1};
+       
+        dfs(sr,sc,ans,image,delrow,delcol,inicolor,color);
+        return ans;
     }
+
+
 };
 
 //{ Driver Code Starts.
