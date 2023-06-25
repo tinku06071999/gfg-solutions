d84f8740d6a31fe5c455b5e97e23015307712a2c
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-06-26 00:23:59 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/nCr/n-cr.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+
+    int nCr(int n, int r){
+        // code here
+          int mod=1000000007;
+        vector<int> dp(r+1,0);
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=min(i,r);j++){
+                if(i>=r){
+                    dp[j]=(dp[j]+dp[j-1])%mod;
+                }
+            }
+        }
+        return dp[r];
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, r;
+        cin>>n>>r;
+        
+        Solution ob;
+        cout<<ob.nCr(n, r)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
