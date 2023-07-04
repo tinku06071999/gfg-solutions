fab0f2680093a266720b3b4043483a150d40287c
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-07-04 19:01:42 +0530

    Updated Solution - GfG to GitHub

--- Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.cpp
+++ Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.cpp
@@ -10,66 +10,65 @@ class Solution{
 public:
 	int search(string pat, string txt) {
 	    // code here
-// 	    unordered_map<char,int>mp;
-// 	    for(int i=0;i<pat.size();i++){
-// 	        mp[pat[i]]++;
-// 	    }
-// 	    int n = txt.size();
-// 	    int count = mp.size();
-// 	    int w = pat.size();
-// 	    int i=0;
-// 	    int j=0;
-// 	    int ans=0;
-// 	    while(j<n){
-// 	        if(mp.count(txt[j])){
-// 	            mp[txt[j]]--;
-// 	            if(mp[txt[j]]==0)count--;
-// 	        }
-// 	        if(j-i+1<w)j++;
-// 	        else if(j-i+1==w){
-// 	            if(count==0){
-// 	                ans++;
-// 	            }
-// 	        if(mp.count(txt[i])){
-// 	                if(mp[txt[i]]==1)count++;
-// 	                mp[txt[i]]++;
-// 	            }
-// 	            i++;
-// 	            j++;
-// 	    }
-// 	}
-// 	    return ans;
-   int patlen=pat.length();
-        int txtlen=txt.length();
-        int i=0;
-        int j=0;
-        
-        unordered_map<char,int> freq;
-        for(int i=0;i<patlen;i++)freq[pat[i]]++;
-        int count=freq.size();
-        int ans=0;
-        
-        while(j<txtlen)
-        {
-            if(freq.count(txt[j]))
-            {
-                freq[txt[j]]--;
-                if(freq[txt[j]]==0)count--;
-            }
-            if(j-i+1<patlen)j++;
-            else if(j-i+1==patlen)
-            {
-                if(count==0)ans++;
-                if(freq.count(txt[i]))
-                {
-                    freq[txt[i]]++;
-                    if(freq[txt[i]]==1)count++;
-                }
-                i++;
-                j++;
-            }
-        }
-        return ans;
+	    unordered_map<char,int>mp;
+	    for(int i=0;i<pat.size();i++){
+	        mp[pat[i]]++;
+	    }
+	    int n = txt.size();
+	    int count = mp.size();
+	    int w = pat.size();
+	    int i=0;
+	    int j=0;
+	    int ans=0;
+	    while(j<n){
+	        if(mp.count(txt[j])){
+	            mp[txt[j]]--;
+	            if(mp[txt[j]]==0)count--;
+	        }
+	        if(j-i+1<w)j++;
+	        else if(j-i+1==w){
+	            if(count==0){
+	                ans++;
+	            }
+	        if(mp.count(txt[i])){
+	               mp[txt[i]]++;
+	                if(mp[txt[i]]==1)count++;
+	                
+	            }
+	            i++;
+	            j++;
+	    }
+	}
+	    return ans;
+        // int w=pat.size();
+        // unordered_map<char,int> mp;
+        // for(int i=0;i<w;i++)mp[pat[i]]++;
+        // int count=mp.size();
+        // int ans=0;
+        // int n=txt.size();
+        // int i=0;
+        // int j=0;
+        // while(j<n)
+        // {
+        //     if(mp.count(txt[j]))
+        //     {
+        //         mp[txt[j]]--;
+        //         if(mp[txt[j]]==0)count--;
+        //     }
+        //     if(j-i+1<w)j++;
+        //     else if(j-i+1==w)
+        //     {
+        //         if(count==0)ans++;
+        //         if(mp.count(txt[i]))
+        //         {
+        //             mp[txt[i]]++;
+        //             if(mp[txt[i]]==1)count++;
+        //         }
+        //         i++;
+        //         j++;
+        //     }
+        // }
+        // return ans;
 	}
 
 };
