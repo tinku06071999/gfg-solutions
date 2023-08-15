bc6715b94748b618f80ad08ca487d7c4e1c3c0ed
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-08-15 11:45:09 +0530

    Updated Solution - GfG to GitHub

--- Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.cpp
+++ Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.cpp
@@ -10,35 +10,32 @@ class Solution{
 public:
 	int search(string pat, string txt) {
 	    // code here
+	    int n=txt.size();
+	    int w = pat.size();
+	    int ans=0;
 	    unordered_map<char,int>mp;
-	    for(int i=0;i<pat.size();i++){
+	    for(int i=0;i<w;i++){
 	        mp[pat[i]]++;
 	    }
-	    int n = txt.size();
-	    int count = mp.size();
-	    int w = pat.size();
-	    int i=0;
-	    int j=0;
-	    int ans=0;
+	    int i=0,j=0;
+	    int cnt = mp.size();
 	    while(j<n){
 	        if(mp.count(txt[j])){
 	            mp[txt[j]]--;
-	            if(mp[txt[j]]==0)count--;
+	            if(mp[txt[j]]==0)cnt--;
 	        }
 	        if(j-i+1<w)j++;
 	        else if(j-i+1==w){
-	            if(count==0){
-	                ans++;
-	            }
-	        if(mp.count(txt[i])){
-	               mp[txt[i]]++;
-	                if(mp[txt[i]]==1)count++;
-	                
-	            }
-	            i++;
-	            j++;
+	            if(cnt==0)ans++;
+	            if(mp.count(txt[i])){
+	                mp[txt[i]]++;
+	                if(mp[txt[i]]==1)cnt++;
+	            } 
+	        i++;
+	        j++;
+	        }
+	       
 	    }
-	}
 	    return ans;
 	}
 
