bef4b12a5833ddb41e562e8d28f28c3c6926e6c9
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-09-16 17:29:17 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count number of hops/count-number-of-hops.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution
+{
+   
+    public:
+    //Function to count the number of ways in which frog can reach the top.
+    int mod = 1e9+7;
+     long long solve(int n,vector<long long>& dp){
+        if(n<0)return 0;
+        if(n==0)return 1;
+        if(dp[n] != -1)return dp[n];
+        return dp[n]=( solve(n-1,dp)+solve(n-2,dp)+solve(n-3,dp))%mod;
+        
+    }
+    long long countWays(int n)
+    {
+        
+        // your code here
+        vector<long long>dp(n+1,-1);
+        return solve(n,dp);
+        
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking testcases
+	int t;
+	cin >> t;
+	
+	while(t--)
+	{
+	    //taking number of steps in stair
+		int n;
+		cin>>n;
+		Solution ob;
+		//calling function countWays()
+		cout << ob.countWays(n) << endl;
+	}
+    
+    return 0;
+    
+}
+
+// } Driver Code Ends
