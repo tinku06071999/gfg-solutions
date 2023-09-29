0f1cfd8a27419f630172a06cc658112a961b8cb3
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-09-29 15:51:22 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number Of Enclaves/number-of-enclaves.cpp
+++ Medium/Number Of Enclaves/number-of-enclaves.cpp
@@ -9,45 +9,32 @@ using namespace std;
 // User function Template for C++
 
 class Solution {
-    private:
-    void dfs(int sr, int sc,vector<vector<int>> &grid, int delrow[], int delcol[],int n, int m){
-     grid[sr][sc]=0;
-     for(int i=0;i<4;i++){
-         int nrow = sr+delrow[i];
-         int ncol = sc+delcol[i];
-         if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]==1 ){
-             dfs(nrow,ncol,grid,delrow,delcol,n,m);
-         }
-     }
-  }
+  void dfs(int sr, int sc, vector<vector<int>> &grid, vector<int>delrow, vector<int>delcol,int n, int m){
+        grid[sr][sc]=0;
+        for(int i=0;i<4;i++){
+            int nr = sr+delrow[i];
+            int nc = sc+delcol[i];
+            if(nc>=0 && nr>=0 && nc<m && nr<n && grid[nr][nc]==1)dfs(nr,nc,grid,delrow,delcol,n,m);
+        }
+    }
   public:
     int numberOfEnclaves(vector<vector<int>> &grid) {
-        // Code here
-          int n = grid.size();
+        int n = grid.size();
         int m = grid[0].size();
-        int delrow[] = {-1,0,+1,0};
-        int delcol[] = {0,+1,0,-1};
-      
-        for(int i=0;i<m;i++){  // for the traversal of first and last  row
-            if(grid[0][i]==1){
-                dfs(0,i,grid,delrow,delcol,n,m);
-            }
-            if(grid[n-1][i]==1){
-                dfs(n-1,i,grid,delrow,delcol,n,m);
-            }
+        vector<int>delrow = {-1,0,+1,0};
+        vector<int>delcol = {0,+1,0,-1};
+        for(int i=0;i<n;i++){
+            if(grid[i][0]==1)dfs(i,0,grid,delrow,delcol,n,m);
+            if(grid[i][m-1]==1)dfs(i,m-1,grid,delrow,delcol,n,m);
         }
-        for(int j=0;j<n;j++){   // for the traversal of first and last column
-           if(grid[j][0]==1 ){
-               dfs(j,0,grid,delrow,delcol,n,m);
-           }
-           if(grid[j][m-1] ){
-               dfs(j,m-1,grid,delrow,delcol,n,m);
-           }
+        for(int i=0;i<m;i++){
+            if(grid[0][i]==1)dfs(0,i,grid,delrow,delcol,n,m);
+            if(grid[n-1][i]==1)dfs(n-1,i,grid,delrow,delcol,n,m);
         }
         int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1 ) count++;
+                if(grid[i][j]==1)count++;
             }
         }
         return count;
