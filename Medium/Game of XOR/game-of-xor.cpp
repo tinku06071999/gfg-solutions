785e72a4a888b85bb01efe4f6dbc0c0e39a9c587
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-12-18 22:48:07 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Game of XOR/game-of-xor.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int gameOfXor(int n , int arr[]) {
+        // code here
+       int res =0;
+        for(int i=0;i<n;i++){
+          int frequency = (i+1)*(n-i);
+          if(frequency % 2 == 1)
+          res ^=arr[i];
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int A[N];
+        for(int i=0 ; i<N ; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.gameOfXor(N,A) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
